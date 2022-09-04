4b66030fcf98e23d64fcfaecd3cbcedf86d622b0
Nicholas Gamunorwa <nicholas.gamunorwa@cassavasmartech.co.zw>
2022-09-04 07:09:12 +0200

    first commit

---
+++ src/main/java/zw/co/pabuwe/polarbookshop/dispatcherservice/OrderAcceptedMessage.java
@@ -0,0 +1,6 @@
+package zw.co.pabuwe.polarbookshop.dispatcherservice;
+
+public record OrderAcceptedMessage(
+        Long orderId
+) {
+}
---
+++ src/main/java/zw/co/pabuwe/polarbookshop/dispatcherservice/OrderDispatchedMessage.java
@@ -0,0 +1,6 @@
+package zw.co.pabuwe.polarbookshop.dispatcherservice;
+
+public record OrderDispatchedMessage(
+        Long orderId
+) {
+}
